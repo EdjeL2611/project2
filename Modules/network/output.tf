af6ba3a5f140049e0de578569ae492331e093800
edje2611 <e.lyken@live.com>
2026-01-31 23:25:32 +0100

    add subnet_id & subnet name output

--- Modules/network/output.tf
+++ Modules/network/output.tf
@@ -7,3 +7,13 @@ output "azurerm_virtual_network_name" {
     value = azurerm_virtual_network.Vnet.name
     description = "The name of the Virtual Network"
 }
+
+output "azurerm_subnet_id" {    
+    value = azurerm_subnet.subnet.id
+    description = "The ID of the Subnet"
+}
+
+output "azurerm_subnet_name" {    
+    value = azurerm_subnet.subnet.name
+    description = "The name of the Subnet" 
+}
